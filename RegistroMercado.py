c9cae9bcd6721104af5d4ffd6433826e2fb6393c
KevinVargasGS <kevinhvargas1@hotmail.com>
2026-09-10 20:34:09 -0500

    Add files via upload

--- RegistroMercado.py
+++ RegistroMercado.py
@@ -1,43 +1,39 @@
-mercado = ["Arroz","Pasta","Crema de leche", "Platano"]
-print ("Tus productos actuales" ,mercado)
+mercado = ["Arroz", "Pasta", "Crema de leche", "Platano"]
+print("Tus productos actuales:", mercado)
 
 activar = True
 
 while activar:
-    print("\n--MENU DE MERCADO --")
+    print("\n--- MENÚ DE MERCADO ---")
     print("1. Agregar un producto")
     print("2. Observar los productos")
     print("3. Eliminar producto")
     print("4. Salir del sistema")
 
-    opcion = input ("Ingresa la opción que quieras escoger " )
+    opcion = input("Ingresa la opción que quieras escoger: ")
 
-    if opcion =="1":
+    if opcion == "1":
         edadid = input("Ingresa tu edad: ")
         edadR = int(edadid)
-        if edadR >=18:
-            print ("Si tienes la edad mayor para agregar un producto")
-            agregarp = input("Agrega el producto " )
-            mercado.append (agregarp)
-            print ("La actualización quedo:" ,mercado)
+        if edadR >= 18:
+            print("Sí tienes la edad mayor para agregar un producto")
+            agregarp = input("Agrega el producto: ")
+            mercado.append(agregarp)
+            print("La actualización quedó:", mercado)
         else:
-            print ("No puedes agregar no eres mayor de edad")
+            print("No puedes agregar, no eres mayor de edad")
     elif opcion == "2":
-        print ("Esta es tu lista de mercado: " ,mercado)
+        print("Esta es tu lista de mercado:", mercado)
     elif opcion == "3":
-        eliminar = input ("¿Qué producto deseas eliminar? " )
+        eliminar = input("¿Qué producto deseas eliminar? ")
         if eliminar in mercado:
             mercado.remove(eliminar)
-            print ("Ahora tu lista esta asi" , mercado)
+            print("Ahora tu lista está así:", mercado)
         else:
-            print("ese prodcuto no esta en la lista")
-    elif opcion =="4":
-        print ("Gracias por usar nuestro sistema")
+            print("Ese producto no está en la lista")
+    elif opcion == "4":
+        print("Gracias por usar nuestro sistema")
         activar = False
     else:
-        print ("opción inválida")
-
-
-
-
+        print("Opción inválida")
 
